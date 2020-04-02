ba9cca0f1dbeb5cdd5e9de13e23963fd54a37786
Jakub Kucera <Jakub.Kucera@nccuk.com>
2020-04-02 16:45:20 +0100

    input1

---
+++ abaqus_cmd.py
@@ -0,0 +1,12 @@
+
+import subprocess
+#import os
+### GOBO #### this runs command line to run python (3.6==> 3.6)
+def cmd(command):
+    process = subprocess.Popen(command,stdout=subprocess.PIPE, shell=True)
+    proc_stdout = process.communicate()#[0].strip()
+    print(command)
+    print(proc_stdout)
+    
+
+#cmd("abaqus cae noGUI=abaqus_postProc.py")
